4ff4205a5a006f94dcf8dd8d2e838abcb91d1447
SubarYan <ayanchin85@gmail.com>
2025-05-04 11:47:08 +0200

    tests are initiated

---
+++ src/main/java/com/mastermindcms/lessons/TasksHolder.java
@@ -0,0 +1,17 @@
+package com.mastermindcms.lessons;
+
+public class TasksHolder {
+
+    public static int getLucky() {
+        return 7;
+    }
+
+    public static String getHelloWorld(){
+        return "hello world";
+    }
+
+    public static int getNumber10(){
+        return 10;
+    }
+
+}
